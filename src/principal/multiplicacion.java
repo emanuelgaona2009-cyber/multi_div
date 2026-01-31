58989c1f61350b7cabc5aa007ffc17211fd4504a
Pc9 <Pc9@villa9>
2026-01-31 10:28:49 -0600

    CodigoMulti

---
+++ src/principal/multiplicacion.java
@@ -0,0 +1,10 @@
+
+package principal;
+
+import java.util.Scanner;
+public class multiplicacion {
+     Scanner sc = new Scanner (System.in);
+   public int res(int a, int b){
+   return a*b;
+   }
+}
