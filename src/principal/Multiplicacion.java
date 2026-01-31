ffd45c14976a9bbb24a293149f369c8e91937624
Pc9 <Pc9@villa9>
2026-01-31 09:53:23 -0600

    CodigoMultiplicacion

---
+++ src/principal/Multiplicacion.java
@@ -0,0 +1,7 @@
+package principal;
+
+public class Multiplicacion {
+    public int calculadora(int a, int b){
+        return a * b;
+    }
+}
--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -1,22 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package principal;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Pc8
- */
 public class Principal {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
         Scanner sc = new Scanner (System.in);
         
         System.out.println("Ingresa el primer numero: ");
