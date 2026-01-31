d3978ec1322ab36497f9c1befeb060a9facd78a2
Pc8 <Pc8@Cpu8>
2026-01-31 09:43:31 -0600

    codigoPrinc

---
+++ src/principal/Principal.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package principal;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Pc8
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner sc = new Scanner (System.in);
+        
+        System.out.println("Ingresa el primer numero: ");
+        int a = sc.nextInt();
+        
+        System.out.println("Ingresa el segundo numero: ");
+        int b = sc.nextInt();
+        
+        /*Multi multi = new Multi();
+        System.out.println("El resultado de la multiplicación es: "+multi.res(a, b));*/
+        
+    }
+    
+}
